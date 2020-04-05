ed99d993d0b97cf060f7aaac77b952a1d126a69f
devleni <50588358+devleni@users.noreply.github.com>
2020-04-05 16:22:22 +0200

    update ui, add createPages api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "gatsby";
 import Nav from "../components/nav";
 import Featured from "../components/featured";
 import Home from "../components/home"
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import "./index.css";
 
 const IndexPage = () => (
   <Layout>
@@ -11,6 +13,7 @@ const IndexPage = () => (
     <Nav />
     <Featured />
     <Home />
+    <Link to="/blog" className="viewmore">View More</Link>
   </Layout>
 )
 
